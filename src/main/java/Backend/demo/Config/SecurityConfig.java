19da65235d992d84873ef7c6530a148634a21f01
ANOOP1YADAV <anoopyadav5984@gmail.com>
2025-11-28 17:54:20 +0530

    Security file changes domain changed

--- src/main/java/Backend/demo/Config/SecurityConfig.java
+++ src/main/java/Backend/demo/Config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "https://your-frontend-domain.com"
+                "https://anime-frontend-beta.vercel.app"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
